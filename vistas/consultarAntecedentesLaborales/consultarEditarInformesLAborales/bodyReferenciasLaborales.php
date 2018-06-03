17c59bab6e76da98c63d66dad41da02cbe4aa4e8
Pablo Bertoli <berto2904@gmail.com>
2018-06-02 23:50:45 -0300

    VAlidacion de Existencia de Referencias Laborales

--- vistas/consultarAntecedentesLaborales/consultarEditarInformesLAborales/bodyReferenciasLaborales.php
+++ vistas/consultarAntecedentesLaborales/consultarEditarInformesLAborales/bodyReferenciasLaborales.php
@@ -1,9 +1,20 @@
+<?php
+  $server = ($_SERVER['DOCUMENT_ROOT']);
+  require ($server.'/sps/clases/ReferenciaLaboral.php');
+  $refLaborales = ReferenciaLaboral::consultarReferenciasLaboralesByIdEntrevista($idEntrevista);
+  // print_r($refLaborales);
+  // die();
+?>
 <div class="tab-pane active" role="tabpanel" id="formuInfoReferenciasLaborales" class="container">
   </div>
   <h3>Informes Laborales</h3>
     <div class="formuInfoReferenciasLaborales row ">
       <div class="col-md-12" style=" display: flex; flex-direction: row; justify-content: space-around;">
-        <div id="empresa_0" class="card col-md-4">
+        <?php
+        if ($refLaborales[0]['id_referencias_laborales'] != ""  ) {
+          foreach ($refLaborales as $key => $ref) {
+          ?>
+         <div id="empresa_<?php echo $key ?>" class="card col-md-4">
           <div class="empresaDescripcion">
             <b>Empresa:</b> <p id="empresa"></p>
           </div>
@@ -17,49 +28,21 @@
             <b>Fecha de Egreso:</b> <p id="hasta" class="desdeHasta"></p>
           </div>
           <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-file"></i> Imprimir </button>
-            <button type="button" class="btn btn-sm btn-success"><i class="glyphicon glyphicon-pencil"></i> Crear/Editar </button>
-            <button type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i> Eliminar </button>
+            <button type="button" class="btn btn-sm btn-primary" onclick="imprimirInformeLaboral(<?php echo $ref['id_referencias_laborales'] ?>)"><i class="glyphicon glyphicon-print"></i> Imprimir </button>
+            <button type="button" class="btn btn-sm btn-success" onclick="crearInformeLaboral(<?php echo $ref['id_referencias_laborales'] ?>)"><i class="glyphicon glyphicon-pencil"></i> Crear/Editar </button>
+            <button type="button" class="btn btn-sm btn-danger"  onclick="eliminarInformeLaboral(<?php echo $ref['id_referencias_laborales'] ?>)"><i class="glyphicon glyphicon-trash"></i> Eliminar </button>
           </div>
         </div>
-        <div id="empresa_1" class="card col-md-4">
-          <div class="empresaDescripcion">
-            <b>Empresa:</b> <p id="empresa"></p>
-          </div>
-          <div class="empresaDescripcion">
-            <b>Domicilio:</b> <p id="domicilio"></p>
-          </div>
-          <div class="empresaDescripcion">
-            <b>Fecha de Ingreso:</b> <p id="desde" class="desdeHasta"></p>
-          </div>
-          <div class="empresaDescripcion">
-            <b>Fecha de Egreso:</b> <p id="hasta" class="desdeHasta"></p>
-          </div>
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-file"></i> Imprimir </button>
-            <button type="button" class="btn btn-sm btn-success"><i class="glyphicon glyphicon-pencil"></i> Crear/Editar </button>
-            <button type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i> Eliminar </button>
-          </div>
+        <?php }
+      }else {
+        ?>
+        <div class="alert alert-danger" role="alert">
+          <i class="glyphicon glyphicon-exclamation-sign"></i>
+          El postulante no dispone de referencias laborales
         </div>
-        <div id="empresa_2" class="card col-md-4">
-          <div class="empresaDescripcion">
-            <b>Empresa:</b> <p id="empresa"></p>
-          </div>
-          <div class="empresaDescripcion">
-            <b>Domicilio:</b> <p id="domicilio"></p>
-          </div>
-          <div class="empresaDescripcion">
-            <b>Fecha de Ingreso:</b> <p id="desde" class="desdeHasta"></p>
-          </div>
-          <div class="empresaDescripcion">
-            <b>Fecha de Egreso:</b> <p id="hasta" class="desdeHasta"></p>
-          </div>
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-file"></i> Imprimir </button>
-            <button type="button" class="btn btn-sm btn-success"><i class="glyphicon glyphicon-pencil"></i> Crear/Editar </button>
-            <button type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i> Eliminar </button>
-          </div>
-        </div>
+        <?php
+      }
+        ?>
       </div>
     </div>
   </div>
