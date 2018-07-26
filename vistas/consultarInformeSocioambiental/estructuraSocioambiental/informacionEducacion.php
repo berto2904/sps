31dbf8c3e6fee61166e02ba8005875f0a939635f
Pablo Bertoli <berto2904@gmail.com>
2018-07-26 01:37:46 -0300

    Introduccion a informacion educacion

--- vistas/consultarInformeSocioambiental/estructuraSocioambiental/informacionEducacion.php
+++ vistas/consultarInformeSocioambiental/estructuraSocioambiental/informacionEducacion.php
@@ -1,71 +1,62 @@
 <div class="titulo">
 </div>
-<h2><strong><em><u>Familiares</u></em></strong></h2>
-<div class="datosFamiliares margin0 both">
-  <?php
-  // print_r($datosFamiliares['Familiares']);
-  // die();
-   ?>
+<h2><strong><em><u>Educacion</u></em></strong></h2>
+<div class="datosFamiliares margin0 both"style="height: 45%; ">
   <table>
     <thead>
       <tr>
-        <th>Familiar</th>
-        <th>Apellido y Nombre</th>
-        <th>Domicilio</th>
-        <th>Profesión</th>
+        <th data-field="stargazers_count" data-sortable="true" rowspan="2">Nivel</th>
+        <th rowspan="2">Establecimiento</th>
+        <th colspan="2">Fecha</th>
+        <th rowspan="2">Situacion</th>
+        <th rowspan="2">Titulo Obtenido</th>
+      </tr>
+      <tr>
+        <th>Desde (Año)</th>
+        <th>Hasta (Año)</th>
       </tr>
     </thead>
     <tbody>
-      <?php
-        foreach ($datosFamiliares['Familiares'] as $key => $familiar) {
-      ?>
       <tr>
-        <td class="tipoFamilia"><?php echo $familiar['familiar_tipo'] ?></td>
-        <td><?php echo $familiar['familiar_apellido_nombre'] ?></td>
-        <td><?php echo $familiar['familiar_domicilio'] ?></td>
-        <td><?php echo $familiar['familiar_profesion'] ?></td>
+        <td class="tipoFamilia">Primario</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
       </tr>
-      <?php
-        }
-      ?>
+      <tr>
+        <td class="tipoFamilia">Secundario</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td class="tipoFamilia">Terciario</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td class="tipoFamilia">Universitario</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td class="tipoFamilia">Otros</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
     </tbody>
   </table>
-</div>
-<br>
-<h3><strong><em><u>Datos del Cónyuge</u></em></strong></h3>
-<?php
-if (isset($datosFamiliares['id_conyuge'])) {
-?>
-  <div class="datosConyuge margin0 both">
-    <p>Nombre: <strong><?php echo $datosFamiliares['conyuge_nombres'] ?></strong> </p>
-    <p>Apellido: <strong><?php echo $datosFamiliares['conyuge_apellido'] ?></strong> </p>
-    <p>Fecha Nacimiento: <strong><?php echo $datosFamiliares['conyuge_fecha_nacimiento'] ?></strong> </p>
-    <p>Lugar Nacimiento: <strong><?php echo $datosFamiliares['conyuge_lugar_nacimiento'] ?></strong> </p>
-    <p>Nacionalidad: <strong><?php echo $datosFamiliares['conyuge_nacionalidad'] ?></strong> </p>
-    <p>Profesion: <strong><?php echo $datosFamiliares['conyuge_profesion'] ?></strong> </p>
-    <p>DNI: <strong><?php echo $datosFamiliares['conyuge_dni'] ?></strong> </p>
-    <p>Sexo: <strong><?php echo $datosFamiliares['conyuge_sexo'] ?></strong> </p>
-  </div>
-<?php
-}
-else {
-  ?>
-  <p>La persona entrevistada no tiene Cónyuge</p>
-  <?php
-}
- ?>
-<br>
-<p><strong><u>Otras personas que conviven con el entrevistado:</u></strong></p>
-<div class="convivencia both margin0">
-<?php
-  if (isset($datosFamiliares['observaciones_convivencia'])) {
-    ?>
-    <p><?php echo $datosFamiliares['observaciones_convivencia'] ?></p>
-    <?php
-  } else {
-    ?>
-    <p>Sin comentarios</p>
-    <?php
-  }
- ?>
 </div>
