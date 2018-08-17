9d044e4c7776c3cf4be7048d0e813ef762009a48
Pablo Bertoli <berto2904@gmail.com>
2018-08-17 15:54:22 -0300

    utf8

--- vistas/headersFooters/headerLibrerias.php
+++ vistas/headersFooters/headerLibrerias.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>RH Global</title>
 	<link rel="icon" type="image/png" href="../librerias/Login/images/icons/rh.ico"/>
