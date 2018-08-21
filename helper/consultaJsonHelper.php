5768238d96bc7a8793fd8a5179cad320f200ec46
Pablo Bertoli <berto2904@gmail.com>
2018-08-21 17:01:48 -0300

    intento 6

--- helper/consultaJsonHelper.php
+++ helper/consultaJsonHelper.php
@@ -8,7 +8,7 @@ function jsonConverter($key,$consultaDecoded){
     // $string = "{";
     foreach ($consultaDecoded as $indice => $value) {
       foreach ($value as $indice2 => $value2) {
-        // utf8_encode_deep($value2);
+        utf8_encode_deep($value2);
         $string.= json_encode($value2);
         if ($indice2 != count($value)-1) {
           $string.= ",";
@@ -22,7 +22,7 @@ function jsonConverterArray($key,$consultaDecoded){
   $string = "";
     $string .= "[";
     foreach ($consultaDecoded as $indice => $value) {
-        // utf8_encode_deep($value);
+        utf8_encode_deep($value);
         $string.= json_encode($value);
         if ($indice != count($consultaDecoded)-1) {
           $string.= ",";
